6fa17f7d476c638a9d4e0d5e4b955f541a7f4f11
Yarnold2002 <yarnoldmurcia@gmail.com>
2025-08-12 10:35:48 -0500

    Se crea la funcionalidad para que el examen sea unico y que los resultados respondieran a un unico examen, tambien el examnen es totalmente aletorio

--- src/modules/students/exams/controller.ts
+++ src/modules/students/exams/controller.ts
@@ -14,3 +14,18 @@ export async function getExamsByVoucher(voucher_id: number) {
 
   return exams;
 }
+
+export async function getRandomExamForVoucher(voucher_id: number) {
+  if (!voucher_id || typeof voucher_id !== "number") {
+    throw new Error("voucher_id invalido");
+  }
+
+  const table = new ExamsTable();
+  const exam = await table.getRandomActiveExam(voucher_id);
+
+  if (!exam) {
+    throw new Error("No se encontró un examen disponible");
+  }
+
+  return exam;
+}
