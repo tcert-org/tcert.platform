deb450679a3ac95eab99c8fd60ffb15407fb08b7
Yarnold2002 <yarnoldmurcia@gmail.com>
2025-08-20 11:14:40 -0500

    Se crea el metodo post para agregar certificaciones

--- src/app/api/certifications/route.ts
+++ src/app/api/certifications/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
   try {
     const { data, error } = await supabase
       .from("certifications")
-      .select("id, name, description, logo_url, active")
+      .select("id, name, description, logo_url, active, study_material_url")
       .order("id", { ascending: true });
 
     if (error) {
@@ -23,6 +23,51 @@ export async function GET() {
     );
   } catch (error) {
     console.error("[CERTIFICATIONS_API_ERROR]", error);
+    return NextResponse.json(
+      { error: "Error interno del servidor" },
+      { status: 500 }
+    );
+  }
+}
+
+// POST - Crear una nueva certificación
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, description, logo_url, active, study_material_url } = body;
+
+    if (
+      !name ||
+      !description ||
+      !logo_url ||
+      typeof active !== "boolean" ||
+      !study_material_url
+    ) {
+      return NextResponse.json(
+        { error: "Faltan campos obligatorios" },
+        { status: 400 }
+      );
+    }
+
+    const { data, error } = await supabase
+      .from("certifications")
+      .insert([{ name, description, logo_url, active, study_material_url }])
+      .select();
+
+    if (error) {
+      console.error("[POST_CERTIFICATION_ERROR]", error.message);
+      return NextResponse.json(
+        { error: "Error al crear la certificación" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, data: data?.[0] },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("[POST_CERTIFICATION_API_ERROR]", error);
     return NextResponse.json(
       { error: "Error interno del servidor" },
       { status: 500 }
